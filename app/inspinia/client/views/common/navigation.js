7d558b5027e3e544282353ade51563faa67c6247
Gislainy Crisostomo Velasco <gislainycrisostomo@gmail.com>
2016-10-03 19:51:02 -0300

    mudança no navigation

--- app/inspinia/client/views/common/navigation.js
+++ app/inspinia/client/views/common/navigation.js
@@ -1,15 +1,50 @@
-Template.navigation.rendered = function(){
+Template.navigation.rendered = function () {
 
-    // Initialize metisMenu
-    $('#side-menu').metisMenu();
+	// Initialize metisMenu
+	$('#side-menu').metisMenu();
 
 };
 
 // Used only on OffCanvas layout
 Template.navigation.events({
-
-    'click .close-canvas-menu' : function(){
-        $('body').toggleClass("mini-navbar");
-    }
+	'click .itemMenu': function () {
+		Router.go(this.rota);
+	},
+	'click .close-canvas-menu': function () {
+		$('body').toggleClass("mini-navbar");
+	}
 
 });
+
+Template.navigation.helpers({
+	menuNavigation() {
+		return _menu;
+	}
+})
+
+_menu = [
+	{
+		template: 'home',
+		rota: '/',
+		nome: 'Home',
+		icone: 'fa fa-th-large',
+	},
+	{
+		template: 'atletaList',
+		rota: '/atleta',
+		nome: 'Atleta',
+		icone: 'fa fa-desktop'
+	},
+	{
+		template: 'usuarioList',
+		rota: '/usuario',
+		nome: 'Usuario',
+		icone: 'fa fa-diamond'
+	},
+	{
+		template: 'estoqueList',
+		rota: '/estoque',
+		nome: 'Estoque',
+		icone: 'fa fa-envelope'
+	},
+]
