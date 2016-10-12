932866c4389cfcd303f645269ca8009acb9a0124
MarcelloVSilva <mvnota10@gmail.com>
2016-10-12 15:27:50 -0300

    modalidades jogos

---
+++ app/modalidades/client/jogosList.js
@@ -0,0 +1,36 @@
+Template.jogosList.helpers({
+	jogosList() {
+		return _jogosList;
+	}
+});
+
+var _jogosList = [
+	{
+		data: '20/10',
+		hora: '18:30h',
+		local: 'ferreira pacheco',
+		timea: 'CC',
+		timeb: 'SI'
+	},
+	{
+		data: '20/10',
+		hora: '18:30h',
+		local: 'ferreira pacheco',
+		timea: 'CC',
+		timeb: 'SI'
+	},
+	{
+		data: '20/10',
+		hora: '18:30h',
+		local: 'ferreira pacheco',
+		timea: 'CC',
+		timeb: 'SI'
+	},
+	{
+		data: '20/10',
+		hora: '18:30h',
+		local: 'ferreira pacheco',
+		timea: 'CC',
+		timeb: 'SI'
+	},
+];
