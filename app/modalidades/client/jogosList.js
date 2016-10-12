01f16d99c19d60dbcca9ba17d517a7660b54c786
MarcelloVSilva <mvnota10@gmail.com>
2016-10-12 17:26:19 -0300

    mod

--- app/modalidades/client/jogosList.js
+++ app/modalidades/client/jogosList.js
@@ -13,22 +13,22 @@ var _jogosList = [
 		timeb: 'SI'
 	},
 	{
-		data: '20/10',
+		data: '21/10',
 		hora: '18:30h',
+		local: 'ferreira pacheco',
+		timea: 'ES',
+		timeb: 'SI'
+	},
+	{
+		data: '23/10',
+		hora: '08:30h',
 		local: 'ferreira pacheco',
 		timea: 'CC',
 		timeb: 'SI'
 	},
 	{
-		data: '20/10',
-		hora: '18:30h',
-		local: 'ferreira pacheco',
-		timea: 'CC',
-		timeb: 'SI'
-	},
-	{
-		data: '20/10',
-		hora: '18:30h',
+		data: '23/10',
+		hora: '14:00h',
 		local: 'ferreira pacheco',
 		timea: 'CC',
 		timeb: 'SI'
